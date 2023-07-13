2ace0601a1cca828a3c4297b554e27c7021dd864
aryannnnhere <aryanskbyadav@gmail.com>
2023-07-14 00:03:09 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/PollingSystem_Api").then(() => {
+  
+  console.log("database successfully connected");
+});
+
+module.exports = mongoose;
